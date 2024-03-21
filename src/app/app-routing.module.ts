36d4b165b360962aa22fd17dfc0c79e4cb01e727
softwarerockstar <mharoon@softwarerockstar.com>
2024-03-21 16:21:48 -0500

    added fax form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyFormComponent } from './my-form/my-form.component';
 import { CmsComponent } from './cms/cms.component';
+import { FaxComponent } from './fax-form/fax-form.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/my-form', pathMatch: 'full'  },
   { path: 'my-form', component: MyFormComponent },
-  {path: 'cms', component: CmsComponent }
+  {path: 'cms', component: CmsComponent },
+  {path: 'fax', component: FaxComponent }
 ];  
 
 
---
+++ src/app/fax-form/fax-form.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-fax',
+  templateUrl: './fax-form.component.html',
+  styleUrls: ['./fax-form.component.scss'],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ]
+})
+export class FaxComponent {
+  faxForm: FormGroup;
+  showFaxInput = true;
+  faxNumber?: string;
+
+  constructor(private fb: FormBuilder) {
+    this.faxForm = this.fb.group({
+      faxInput: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]]
+    });
+  }
+
+  sendFax() {
+    if (this.faxForm.valid) {
+      this.showFaxInput = false;
+      this.faxNumber = this.faxForm.get('faxInput')?.value;
+    }
+  }
+}
